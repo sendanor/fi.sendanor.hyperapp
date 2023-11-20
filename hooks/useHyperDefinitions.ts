7a667b2a99b6033b698354c6850debc878414233
Jaakko Heusala <jhh@hg.fi>
2023-11-21 00:18:02 +0200

    Changed hyper app to populate remote resources https://github.com/sendanor/hyperstack/issues/35

--- hooks/useHyperDefinitions.ts
+++ hooks/useHyperDefinitions.ts
@@ -5,19 +5,23 @@ import { HttpService } from "../../../hg/core/HttpService";
 import { isString } from "../../../hg/core/types/String";
 import { RefreshCallback, useAsyncResource } from "../../../hg/frontend/hooks/useAsyncResource";
 import { explainHyperDTO, HyperDTO, isHyperDTO } from "../../hyperstack/dto/HyperDTO";
+import { populateHyperDTO } from "../../hyperstack/utils/populateHyperDTO";
 
 export function useHyperDefinitions (
     definitions : HyperDTO | string
 ) : [HyperDTO | null | undefined, RefreshCallback] {
     const callback = useCallback( async () => {
+
         if (!isString(definitions)) {
-            return definitions;
+            return await populateHyperDTO(definitions);
         }
+
         const result = await HttpService.getJson(definitions);
         if (!isHyperDTO(result)) {
             throw new TypeError(`The response was not HyperDTO: ${explainHyperDTO(result)}`)
         }
-        return result;
+        return await populateHyperDTO(result, result?.publicUrl ?? definitions);
+
     }, [
         definitions
     ]);
